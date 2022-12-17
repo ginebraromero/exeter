0204f865630e7f989e56882daf2f3a225187ce35
grommar847j <118803800+grommar847j@users.noreply.github.com>
2022-12-17 16:05:28 +0100

    First upload

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+/*Hace que la barra se esconda cuando se hace scroll*/
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
